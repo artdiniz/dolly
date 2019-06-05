afcadf468c1072ca370415c7c1cce23ce7cc1fda
Artur Diniz Adam <artur.daz@gmail.com>
2019-06-05 18:27:58 -0300

    Multiline exercise statements

--- src/exercise/markdown.ts
+++ src/exercise/markdown.ts
@@ -1,44 +1,44 @@
 import { html as code } from 'common-tags'
 import { IExerciseChapter, IExerciseStepsItem } from 'exercise/@types'
 
-export function toCodeStepMarkdown(item: IExerciseStepsItem): string {
+export function toExerciseStepMarkdown(item: IExerciseStepsItem): string {
+  const statement = `${item.position}. ${item.statement}`
   if ('codeChanges' in item) {
     return code`
-      ${item.position}. ${item.statement}
+      ${statement}
 
       ${item.codeChanges
         .map(
           change => code`
             ####### ${change.filePath}
       
-            \`\`\`${change.codeLanguage}
-            ${code(change.code).trim()}
-            \`\`\`          
+              \`\`\`${change.codeLanguage}
+              ${code(change.code).trim()}
+              \`\`\`
           `
         )
         .join('\n\n')}
     `
   } else {
-    return code`
-      ${item.position}. ${item.statement}
-    `
+    return statement
   }
 }
 
 export function toChapterMarkdown(exerciseChapter: IExerciseChapter): string {
   const introMarkdown = code`
-        # Exercício: ${exerciseChapter.title}
+    # Exercício: ${exerciseChapter.title}
 
-        ## Objetivo
+    ## Objetivo
 
-        ${exerciseChapter.objective}
+    ${exerciseChapter.objective}
 
-        ## Passo a passo com código
-    `
+    ## Passo a passo com código
+  `
 
   const exerciseItemsMarkdown = exerciseChapter.steps
-    .map(item => toCodeStepMarkdown(item))
+    .map(item => toExerciseStepMarkdown(item))
     .join('\n\n')
+    .trim()
 
-  return introMarkdown.trim() + '\n\n' + exerciseItemsMarkdown.trim()
+  return introMarkdown + '\n\n' + exerciseItemsMarkdown + '\n'
 }
