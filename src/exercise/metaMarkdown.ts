f8ef087ee431d94f0ffd1160f0e839ff9a23c3b5
Artur Diniz Adam <artur.daz@gmail.com>
2019-06-08 04:34:11 -0300

    Defining meta markdown steps markup

--- src/exercise/metaMarkdown.ts
+++ src/exercise/metaMarkdown.ts
@@ -2,16 +2,36 @@ import { html as code } from 'common-tags'
 
 import { IExerciseChapter, IExerciseStepsItem } from 'exercise/@types'
 
-function toStepsMetaMarkdown(steps: IExerciseStepsItem[]): string {
-  return ''
+function toStepsMetaMarkdown(steps: IExerciseStepsItem[]): string[] {
+  return steps.map(step => {
+    const stepHash = '43g576d'
+    const stepChangesMarkdown = step.changes.map(
+      change => code`
+      ####### ${change.type} - ${change.filePath}
+              ${change.statement || 'sem sub-enunciado'}
+      `
+    )
+
+    return code`
+      #=============== ⬇ ${stepHash} ⬇
+
+      ${step.statement}
+      
+         ${stepChangesMarkdown.join('\n')}
+    `
+  })
 }
 
 export function toChapterMetaMarkdown(exerciseChapter: IExerciseChapter): string {
-  const chapterStepsMarkdown = toStepsMetaMarkdown(exerciseChapter.steps)
+  const chapterStepsMarkdown = toStepsMetaMarkdown(exerciseChapter.steps).join(
+    '\n\n\n\n\n'
+  )
   chapterStepsMarkdown
   return code`
     ${exerciseChapter.title}
     ---
     ${exerciseChapter.objective}
+    ${chapterStepsMarkdown ? '---\n\n\n' : ''}
+    ${chapterStepsMarkdown || ''}
    `
 }
