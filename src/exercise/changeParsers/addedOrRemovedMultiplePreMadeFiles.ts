26724b31ed79ccedacc7b4ee9687af579e14c563
Artur Diniz Adam <artur.daz@gmail.com>
2019-12-02 18:58:33 -0300

    More declarative pre made files statements

--- src/exercise/changeParsers/addedOrRemovedMultiplePreMadeFiles.ts
+++ src/exercise/changeParsers/addedOrRemovedMultiplePreMadeFiles.ts
@@ -2,6 +2,8 @@ import treeify from 'treeify'
 
 import { html as code } from 'common-tags'
 
+import _partiton from 'lodash/partition'
+
 import { IChange, IChangeAddedFile, IChangeDeletedFile } from 'changes/@types'
 import { IExerciseStepsItem } from 'exercise/@types'
 import { IChangeToExerciseItemParser } from 'exercise/changeParsers/@types'
@@ -21,83 +23,75 @@ function shouldParse(
 function parse(changes: IChange[], changePosition?: number): IExerciseStepsItem {
   if (!shouldParse(changes)) throw Error('Programming error: Should not be parsing')
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  function toFileTreeObject(subtree: any, path: string) {
-    const splittedPath = path.split('/')
-
-    const currentTree = subtree[splittedPath[0]] || {}
-
-    if (splittedPath.length === 2) {
-      const lastItem = { [splittedPath[1]]: null }
-      subtree[splittedPath[0]] = {
-        ...currentTree,
-        ...lastItem
-      }
-      return subtree
-    }
-
-    if (splittedPath.length > 2) {
-      subtree[splittedPath[0]] = {
-        ...toFileTreeObject(currentTree, splittedPath.slice(1).join('/'))
-      }
-      return subtree
-    }
-  }
-
-  const addedFiles = changes
+  const addedFiles: string[] = changes
     .filter(change => change.type === 'added')
     .map(change => change.filePath)
 
-  const addedFileTree = addedFiles.reduce(toFileTreeObject, {})
+  const addedFileTree = toFileTreeObject(addedFiles)
 
   const deletedFiles = changes
     .filter(change => change.type === 'deleted')
     .map(change => change.filePath)
 
-  const deletedFileTree = deletedFiles.reduce(toFileTreeObject, {})
+  const deletedFileTree = toFileTreeObject(deletedFiles)
 
-  const addedFoldersInRoot = Object.keys(addedFileTree)
-  const deletedFoldersInRoot = Object.keys(deletedFileTree)
+  const deletedFileTreeInfo = getTreeObjectInfo(deletedFileTree)
+  const addedFileTreeInfo = getTreeObjectInfo(addedFileTree)
 
-  const qtAddedFiles = addedFiles.length
-  const addedFilesWord =
-    qtAddedFiles === 1 ? 'arquivo a ser adicionado' : 'arquivos a serem adicionados'
-  const addedFoldersWord =
-    addedFoldersInRoot.length === 1 ? 'na pasta' : 'nas pastas'
-  const addedFoldersAsSentence = Array.from(addedFoldersInRoot)
-    .join(', ')
-    .replace(/, ([^,]+)$/, ' e $1')
+  let addedFilesStatement
+  if (addedFiles.length === 1) {
+    addedFilesStatement = addedSingleFileSentence({
+      fileName: addedFiles[0],
+      folderName: addedFileTreeInfo.rootFolders[0] || 'raiz do projeto'
+    })
+  } else if (addedFiles.length > 1 && addedFileTreeInfo.rootFolders.length === 1) {
+    addedFilesStatement = addedMultipleFilesInSingleFolderSentence({
+      qtFiles: addedFiles.length,
+      folderName: addedFileTreeInfo.rootFolders[0] || 'raiz do projeto'
+    })
+  } else if (addedFiles.length > 1 && addedFileTreeInfo.rootFolders.length > 1) {
+    addedFilesStatement = addedMultipleFilesInMultipleFoldersSentence({
+      qtFiles: addedFiles.length,
+      folderNameList:
+        addedFileTreeInfo.rootFiles.length === 0
+          ? addedFileTreeInfo.rootFolders
+          : [...addedFileTreeInfo.rootFolders, 'raiz do projeto']
+    })
+  }
 
-  const addedFilesStatement =
-    qtAddedFiles !== 0 &&
-    code`
-      ${qtAddedFiles} ${addedFilesWord} ${addedFoldersWord} ${addedFoldersAsSentence}s
-    `.trim()
-
-  const qtDeletedFiles = deletedFiles.length
-  const deletedFilesWord =
-    qtDeletedFiles === 1 ? 'arquivo a ser removido' : 'arquivos a serem removidos'
-  const deletedFoldersWord =
-    deletedFoldersInRoot.length === 1 ? 'da pasta' : 'das pastas'
-  const deletedFoldersAsSentence = Array.from(deletedFoldersInRoot)
-    .join(', ')
-    .replace(/, ([^,]+)$/, ' e $1')
-
-  const deletedFilesStatement =
-    qtDeletedFiles !== 0 &&
-    code`
-      ${qtDeletedFiles} ${deletedFilesWord} ${deletedFoldersWord} ${deletedFoldersAsSentence}
-    `.trim()
+  let deletedFilesStatement
+  if (deletedFiles.length === 1) {
+    deletedFilesStatement = deletedSingleFileSentence({
+      fileName: deletedFiles[0],
+      folderName: deletedFileTreeInfo.rootFolders[0] || 'raiz do projeto'
+    })
+  } else if (
+    deletedFiles.length > 1 &&
+    deletedFileTreeInfo.rootFolders.length === 1
+  ) {
+    deletedFilesStatement = deletedMultipleFilesInSingleFolderSentence({
+      qtFiles: deletedFiles.length,
+      folderName: deletedFileTreeInfo.rootFolders[0] || 'raiz do projeto'
+    })
+  } else if (deletedFiles.length > 1 && deletedFileTreeInfo.rootFolders.length > 1) {
+    deletedFilesStatement = deletedMultipleFilesInMultipleFoldersSentence({
+      qtFiles: deletedFiles.length,
+      folderNameList:
+        deletedFileTreeInfo.rootFiles.length === 0
+          ? deletedFileTreeInfo.rootFolders
+          : [...deletedFileTreeInfo.rootFolders, 'raiz do projeto']
+    })
+  }
 
   const preMadeWarningStatement =
-    qtAddedFiles + qtDeletedFiles === 1
+    addedFiles.length + deletedFiles.length === 1
       ? 'Note que esse arquivo foi disponibilizado já pronto para você.'
       : 'Note que esses arquivos foram disponibilizados já prontos para você.'
 
   const mainStatement =
-    qtAddedFiles > 0 && qtDeletedFiles > 0
+    addedFiles.length > 0 && deletedFiles.length > 0
       ? `Há ${addedFilesStatement} e ${deletedFilesStatement}.`
-      : qtAddedFiles > 0
+      : addedFiles.length > 0
       ? `Há ${addedFilesStatement}.`
       : `Há ${deletedFilesStatement}.`
 
@@ -105,16 +99,16 @@ function parse(changes: IChange[], changePosition?: number): IExerciseStepsItem 
 
   const codeChanges = []
 
-  if (qtAddedFiles > 0) {
+  if (addedFiles.length > 0) {
     const addedCodeChangesStatement = `${
-      qtAddedFiles == 1
+      addedFiles.length == 1
         ? 'O arquivo deve ser adicionado'
-        : `Os ${qtAddedFiles} arquivos devem ser adicionados`
+        : `Os ${addedFiles.length} arquivos devem ser adicionados`
     } na seguinte estrutura de pastas:`
 
     codeChanges.push({
       statement: addedCodeChangesStatement,
-      code: code(treeify.asTree(addedFileTree, false, true))
+      code: code(treeify.asTree(addedFileTree as treeify.TreeObject, false, true))
         .split('\n')
         .map(line => ({
           type: 'context' as 'context',
@@ -126,16 +120,16 @@ function parse(changes: IChange[], changePosition?: number): IExerciseStepsItem 
     })
   }
 
-  if (qtDeletedFiles > 0) {
+  if (deletedFiles.length > 0) {
     const deletedCodeChangesStatement = `${
-      qtDeletedFiles === 1
+      deletedFiles.length === 1
         ? 'O arquivo que deve ser removido se encontra'
-        : `Os ${qtDeletedFiles} arquivos que devem ser removidos se encontram`
+        : `Os ${deletedFiles.length} arquivos que devem ser removidos se encontram`
     } na seguinte estrutura de pastas:`
 
     codeChanges.push({
       statement: deletedCodeChangesStatement,
-      code: code(treeify.asTree(deletedFileTree, false, true))
+      code: code(treeify.asTree(deletedFileTree as treeify.TreeObject, false, true))
         .split('\n')
         .map(line => ({
           type: 'context' as 'context',
@@ -159,4 +153,114 @@ export const addedOrRemovedMultiplePreMadeFiles: IChangeToExerciseItemParser<
 > = {
   shouldParse,
   parse
+}
+
+function addedMultipleFilesInMultipleFoldersSentence({
+  qtFiles,
+  folderNameList
+}: {
+  qtFiles: number
+  folderNameList: string[]
+}) {
+  const foldersSentence = folderNameList.join(', ').replace(/, ([^,]+)$/, ' e $1')
+
+  return `${qtFiles} arquivos a serem adicionados nas pastas ${foldersSentence}`
+}
+
+function addedMultipleFilesInSingleFolderSentence({
+  qtFiles,
+  folderName
+}: {
+  qtFiles: number
+  folderName: string
+}) {
+  return `${qtFiles} arquivos a serem adicionados na pasta ${folderName}`
+}
+
+function addedSingleFileSentence({
+  fileName,
+  folderName
+}: {
+  fileName: string
+  folderName: string
+}) {
+  return `1 arquivo "${fileName}" a ser adicionado na pasta ${folderName}`
+}
+
+function deletedMultipleFilesInMultipleFoldersSentence({
+  qtFiles,
+  folderNameList
+}: {
+  qtFiles: number
+  folderNameList: string[]
+}) {
+  const foldersSentence = folderNameList.join(', ').replace(/, ([^,]+)$/, ' e $1')
+
+  return `${qtFiles} arquivos a serem removidos das pastas ${foldersSentence}`
+}
+
+function deletedMultipleFilesInSingleFolderSentence({
+  qtFiles,
+  folderName
+}: {
+  qtFiles: number
+  folderName: string
+}) {
+  return `${qtFiles} arquivos a serem removidos da pasta ${folderName}`
+}
+
+function deletedSingleFileSentence({
+  fileName,
+  folderName
+}: {
+  fileName: string
+  folderName: string
+}) {
+  return `1 arquivo "${fileName}" a ser removido da pasta ${folderName}`
+}
+
+type ITreeObject = {
+  [key: string]: null | ITreeObject
+}
+
+function getTreeObjectInfo(treeObject: ITreeObject) {
+  const [rootFiles, rootFolders] = _partiton(
+    Object.entries(treeObject['pasta_raiz_do_projeto'] || {}),
+    ([, value]) => value === null
+  )
+
+  return {
+    rootFolders: rootFolders.map(([name]) => name),
+    rootFiles: rootFiles.map(([name]) => name)
+  }
+}
+
+function toFileTreeObject(files: string[]): ITreeObject {
+  return {
+    pasta_raiz_do_projeto: files.reduce(toFileTreeObjectReducer, {})
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function toFileTreeObjectReducer(
+  subtree: ITreeObject = {},
+  path: string
+): ITreeObject {
+  const splittedPath = path.split('/')
+
+  if (splittedPath.length === 1) {
+    subtree[splittedPath[0]] = null
+    return subtree
+  }
+
+  if (splittedPath.length >= 2) {
+    const nextTree = subtree[splittedPath[0]] || {}
+
+    subtree[splittedPath[0]] = {
+      ...toFileTreeObjectReducer(nextTree, splittedPath.slice(1).join('/'))
+    }
+    return subtree
+  }
+
+  return subtree
 }
